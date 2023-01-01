f4bd85428fd5cae9dbff4d3edcad13d71fc9d4e2
dannyp0930 <dannyp0930@gmail.com>
2023-01-01 13:15:35 +0900

    FEAT: 라우터 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Auth from "./routes/Auth";
+import Home from "./routes/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   );
