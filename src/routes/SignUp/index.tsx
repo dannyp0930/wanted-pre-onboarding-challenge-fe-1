20b31c9e1ab9ad7d4c4667556f1d15bc4d80849c
dannyp0930 <dannyp0930@gmail.com>
2023-01-01 14:14:52 +0900

    FEAT: 회원가입 폼 생성

--- src/routes/SignUp/index.tsx
+++ src/routes/SignUp/index.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 function SignUp() {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+  }
   return (
     <div>
       <h1>회원가입</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="이메일"
+          onChange={(e) => setEmail(e.target.value)}
+          pattern="^[a-z0-9]+@[a-z]+\.[a-z]"
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          onChange={(e) => setPassword(e.target.value)}
+          minLength={8}
+        />
+        <button type="submit">생성</button>
+      </form>
     </div>
   );
 }
 
-export default SignUp
+export default SignUp;
