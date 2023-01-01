e711f7ba8ba494ef6ad8c059d33d915d56e434f9
dannyp0930 <dannyp0930@gmail.com>
2023-01-01 14:37:25 +0900

    FEAT: 로그아웃

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   const accesstoken = localStorage.getItem("accesstoken");
+  function handleClick() {
+    localStorage.removeItem("accesstoken");
+    navigate('/auth');
+  }
   return (
-    <>{accesstoken ? <div>Home</div> : <Navigate to="/auth"></Navigate>}</>
+    <>
+      {accesstoken ? (
+        <div>
+          <button onClick={handleClick}>로그아웃</button>
+          <h1>To Do</h1>
+        </div>
+      ) : (
+        <Navigate to="/auth"></Navigate>
+      )}
+    </>
   );
 }
 
