3c0a7cc0828f43b37034c41ed673454e5ed38cb0
dannyp0930 <dannyp0930@gmail.com>
2023-01-01 14:34:15 +0900

    FEAT: 로그인 이후 엑세스 토큰 저장, home으로 이동

--- src/routes/Auth/index.tsx
+++ src/routes/Auth/index.tsx
@@ -1,13 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Auth() {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const navigate = useNavigate();
+  async function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+    const res = await axios.post("http://localhost:8080/users/login", {
+      email: email,
+      password: password,
+    });
+    localStorage.setItem("accesstoken", res.data.token);
+    navigate("/");
+  }
   return (
     <div>
       <h1>로그인</h1>
-      <form>
-        <input type="email" />
-        <input type="password" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="이메일"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>로그인</button>
       </form>
       <Link to="/signup">회원가입</Link>
