4ddcfe47de2c267a82da666f68dffae31fb752a3
dannyp0930 <dannyp0930@gmail.com>
2023-01-01 13:11:11 +0900

    FEAT: react-router-dom 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
