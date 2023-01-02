425f9b1fb0dbf431e36fe0c1a9a7f1ff22d71345
dannyp0930 <dannyp0930@gmail.com>
2023-01-02 15:49:15 +0900

    FEAT: ToDo 관련 api 선언

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -5,8 +5,47 @@ const instance = tokenInstance();
 export async function getToDos() {
   try {
     const res = await instance.get("/todos");
-    console.log(res)
+    console.log(res);
   } catch (error) {
-    console.log(error)
+    console.log(error);
+  }
+}
+
+export async function getTodoById() {
+  try {
+    const res = await instance.get("/todos");
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function createTodo(id: string) {
+  try {
+    const res = await instance.get(`/todos/${id}`);
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateTodo(id: string, title: string, content: string) {
+  try {
+    const res = await instance.put(`/todos/${id}`, {
+      title: title,
+      content: content,
+    });
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteTodo(id: string) {
+  try {
+    const res = await instance.delete(`/todos/${id}`);
+    console.log(res);
+  } catch (error) {
+    console.log(error);
   }
 }
