7edff621e523bd087b94e6e0dc5f056768b6fd3d
dannyp0930 <dannyp0930@gmail.com>
2023-01-02 19:44:58 +0900

    FEAT: global style 초기화

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,23 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+  
+  h2 {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  p { 
+    font-size: 1rem;
+  }
+
   button {
     cursor: pointer;
   }
